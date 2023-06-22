50c4dfd21f21deb5aabad8e16831f893b7df6eb6
shdomi8599 <shdomi@naver.com>
2023-06-23 05:42:20 +0900

    feat : SubSection1 배경 애니메이션

--- src/components/section/SubSection1.tsx
+++ src/components/section/SubSection1.tsx
@@ -1,11 +1,30 @@
 import Image from "next/image";
 import styled from "@emotion/styled";
-import { useRef } from "react";
+import { keyframes } from "@emotion/react";
+import { useEffect, useRef } from "react";
 
 import SubSectionForm from "./SubSectionForm";
 
 const SubSection1 = () => {
   const target = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const boxOffsetTop = target.current?.scrollHeight || 0;
+      const scrollY = window.scrollY;
+
+      const isView = scrollY > boxOffsetTop - 400;
+
+      target.current?.classList.toggle("animate", isView);
+      target.current?.classList.toggle("reverse-animate", !isView);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <SubSectionForm height={1181} zIndex={1}>
       <LUImage src={"/01/01-LU.png"} alt="LU" width={493} height={95} />
@@ -24,6 +43,24 @@ const LUImage = styled(Image)`
   z-index: 1;
 `;
 
+const animateBackground = keyframes`
+  0% {
+    background-position: -100px;
+  }
+  100% {
+    background-position: 0px;
+  }
+`;
+
+const reverseAnimateBackground = keyframes`
+  0% {
+    background-position: 0px;
+  }
+  100% {
+    background-position: -100px;
+  }
+`;
+
 const Box = styled.div`
   display: flex;
   align-items: center;
@@ -31,5 +68,16 @@ const Box = styled.div`
   background: url("/01/01.webp") no-repeat center center;
   background-size: cover;
   margin-right: -100px;
+  background-position: -100px;
   clip-path: polygon(0 110px, 100% 0, 100% 100%, 0 100%);
+
+  &.animate {
+    background-position: 0px;
+    animation: ${animateBackground} 0.3s linear;
+  }
+
+  &.reverse-animate {
+    background-position: -100px;
+    animation: ${reverseAnimateBackground} 0.3s linear;
+  }
 `;
