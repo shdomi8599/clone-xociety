9f12a4ff64f3d13a813d02fe1e41914651a2ebc4
shdomi8599 <shdomi@naver.com>
2023-06-23 05:52:28 +0900

    feat : hook useScrollClassToggle

---
+++ src/hooks/useScrollClassToggle.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+
+export const useScrollClassToggle = () => {
+  const target = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const boxOffsetTop = target.current?.scrollHeight || 0;
+      const scrollY = window.scrollY;
+
+      const isView = scrollY > boxOffsetTop - 700;
+
+      target.current?.classList.toggle("animate", isView);
+      target.current?.classList.toggle("reverse-animate", !isView);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return { target };
+};
