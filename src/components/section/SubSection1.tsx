afb5700f8a1522fab1dee5e848d5b8c84ef105b8
shdomi8599 <shdomi@naver.com>
2023-06-23 04:50:54 +0900

    feat : 컴포넌트 SubSection1

--- src/components/section/SubSection1.tsx
+++ src/components/section/SubSection1.tsx
@@ -1,11 +1,35 @@
+import Image from "next/image";
+import styled from "@emotion/styled";
+import { useRef } from "react";
+
 import SubSectionForm from "./SubSectionForm";
 
 const SubSection1 = () => {
+  const target = useRef<HTMLDivElement>(null);
   return (
     <SubSectionForm height={1181} zIndex={1}>
-      dsa
+      <LUImage src={"/01/01-LU.png"} alt="LU" width={493} height={95} />
+      <Box ref={target}></Box>
     </SubSectionForm>
   );
 };
 
 export default SubSection1;
+
+const LUImage = styled(Image)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 14px;
+  z-index: 1;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  background: url("/01/01.webp") no-repeat center center;
+  background-size: cover;
+  margin-right: -100px;
+  clip-path: polygon(0 110px, 100% 0, 100% 100%, 0 100%);
+`;
