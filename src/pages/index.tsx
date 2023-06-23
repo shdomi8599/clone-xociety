9d1b855e515b472a6f0e2768a51b02777b4ce94b
shdomi8599 <shdomi@naver.com>
2023-06-23 16:52:30 +0900

    feat : SubSection2 배치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Header from "@/components/common/Header";
 import MainSection from "@/components/section/MainSection";
 import SubSection1 from "@/components/section/SubSection1";
+import SubSection2 from "@/components/section/SubSection2";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Header />
       <MainSection />
       <SubSection1 />
+      <SubSection2 />
     </>
   );
 };
